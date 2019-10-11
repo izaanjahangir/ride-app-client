c8bc3d2a9f40b5ac44ab450c2bb6e9073aa6ae30
izaanjahangir <izaanjahangir2@gmail.com>
2019-10-11 22:18:12 +0500

    removed initial route setting

--- navigation/index.js
+++ navigation/index.js
@@ -3,12 +3,9 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import AuthScreen from "./AuthScreens";
 import MainScreens from "./MainScreens";
 
-const AppNavigator = createSwitchNavigator(
-  {
-    Auth: AuthScreen,
-    Main: MainScreens
-  },
-  { initialRouteName: "Main" }
-);
+const AppNavigator = createSwitchNavigator({
+  Auth: AuthScreen,
+  Main: MainScreens
+});
 
 export default createAppContainer(AppNavigator);
